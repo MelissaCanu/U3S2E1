947d120d0f2efdea5fc485fedb2d38d0b59f2f74
MelissaCanu <melissamcanu@gmail.com>
2024-01-08 16:59:48 +0100

    two-column-structure added, working on handleBookSelect function

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,41 +1,77 @@
-import { Component } from 'react'
-import SingleBook from './SingleBook'
-import { Col, Form, Row } from 'react-bootstrap'
+import { Component } from "react";
+import SingleBook from "./SingleBook";
+import CommentArea from "./CommentArea";
+import { Col, Form, Row } from "react-bootstrap";
 
 class BookList extends Component {
-  state = {
-    searchQuery: '',
-  }
+	state = {
+		searchQuery: "" /* traccia il testo inserito nella barra di ricerca */,
+		selectedBook: null /* traccia il libro selezionato dall'utente */,
+	};
 
-  render() {
-    return (
-      <>
-        <Row className="justify-content-center mt-5">
-          <Col xs={12} md={4} className="text-center">
-            <Form.Group>
-              <Form.Control
-                type="search"
-                placeholder="Cerca un libro"
-                value={this.state.searchQuery}
-                onChange={(e) => this.setState({ searchQuery: e.target.value })}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="g-2 mt-3">
-          {this.props.books
-            .filter((b) =>
-              b.title.toLowerCase().includes(this.state.searchQuery)
-            )
-            .map((b) => (
-              <Col xs={12} md={4} key={b.asin}>
-                <SingleBook book={b} />
-              </Col>
-            ))}
-        </Row>
-      </>
-    )
-  }
+	/* creo la funzione per impostare lo stato selectedBook, il libro viene passato come argomento 
+  --- la funzione viene passata al componente SingleBook come prop "onSelect" che gestisce l'evento di selezione di un libro */
+
+	handleBookSelect = (book) => {
+		console.log("Book selected:", book);
+		this.setState({ selectedBook: book });
+	};
+
+	render() {
+		return (
+			/* area con il form search */
+			<>
+				<Row className="justify-content-center mt-5">
+					<Col xs={12} md={4} className="text-center">
+						<Form.Group>
+							<Form.Control
+								type="search"
+								placeholder="Cerca un libro"
+								value={this.state.searchQuery}
+								onChange={(e) => this.setState({ searchQuery: e.target.value })}
+							/>
+						</Form.Group>
+					</Col>
+				</Row>
+
+				{/* qua aggiungo la colonna dx per la commentArea */}
+
+				<Row className="g-2 mt-3">
+					{/* colonna SX -- griglia libri */}
+					<Col xs={12} md={6}>
+						{this.props.books
+							.filter((b) =>
+								b.title.toLowerCase().includes(this.state.searchQuery)
+							)
+							.map((b) => (
+								<SingleBook
+									key={
+										b.asin
+									} /* Questo attributo key è utilizzato da React per identificare in modo univoco ogni elemento all'interno di un elenco. 
+              In questo caso, viene utilizzato l'asin del libro come chiave. */
+									book={
+										b
+									} /*  Questo passa l'oggetto libro b come prop chiamata book al componente SingleBook. 
+              Quindi, all'interno del componente SingleBook, puoi accedere all'oggetto libro utilizzando this.props.book. */
+									onSelect={() =>
+										this.handleBookSelect(b)
+									} /* Questo passa una funzione di callback chiamata onSelect al componente SingleBook. 
+              La funzione di callback è una funzione anonima che, quando chiamata, eseguirà this.handleBookSelect(b), 
+              ovvero imposterà il libro b come libro selezionato nel componente BookList. */
+								/>
+							))}
+					</Col>
+
+					{/* colonna DX -- CommentArea */}
+					<Col xs={12} md={6}>
+						{this.state.selectedBook && (
+							<CommentArea asin={this.state.selectedBook.asin} />
+						)}
+					</Col>
+				</Row>
+			</>
+		);
+	}
 }
 
-export default BookList
+export default BookList;
--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,50 +1,51 @@
-import { Component } from 'react'
-import CommentList from './CommentList'
-import AddComment from './AddComment'
-import Loading from './Loading'
-import Error from './Error'
+import { Component } from "react";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+import Loading from "./Loading";
+import Error from "./Error";
 
 class CommentArea extends Component {
-  state = {
-    comments: [],
-    isLoading: true,
-    isError: false,
-  }
+	state = {
+		comments: [],
+		isLoading: true,
+		isError: false,
+	};
 
-  componentDidMount = async () => {
-    try {
-      let response = await fetch(
-        'https://striveschool-api.herokuapp.com/api/comments/' +
-          this.props.asin,
-        {
-          headers: {
-            Authorization: 'Bearer inserisci-qui-il-tuo-token',
-          },
-        }
-      )
-      console.log(response)
-      if (response.ok) {
-        let comments = await response.json()
-        this.setState({ comments: comments, isLoading: false, isError: false })
-      } else {
-        this.setState({ isLoading: false, isError: true })
-      }
-    } catch (error) {
-      console.log(error)
-      this.setState({ isLoading: false, isError: true })
-    }
-  }
+	componentDidMount = async () => {
+		try {
+			let response = await fetch(
+				"https://striveschool-api.herokuapp.com/api/comments/" +
+					this.props.asin,
+				{
+					headers: {
+						Authorization:
+							"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc1ZjY4YzNkYWRhMDAwMThhNjlmOTgiLCJpYXQiOjE3MDQ3MjgwNjMsImV4cCI6MTcwNTkzNzY2M30.hGU4CJK-L33k0In-Ss73UX1BOtPeRQAxVsevitH9_Vc",
+					},
+				}
+			);
+			console.log(response);
+			if (response.ok) {
+				let comments = await response.json();
+				this.setState({ comments: comments, isLoading: false, isError: false });
+			} else {
+				this.setState({ isLoading: false, isError: true });
+			}
+		} catch (error) {
+			console.log(error);
+			this.setState({ isLoading: false, isError: true });
+		}
+	};
 
-  render() {
-    return (
-      <div className="text-center">
-        {this.state.isLoading && <Loading />}
-        {this.state.isError && <Error />}
-        <AddComment asin={this.props.asin} />
-        <CommentList commentsToShow={this.state.comments} />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="text-center">
+				{this.state.isLoading && <Loading />}
+				{this.state.isError && <Error />}
+				<AddComment asin={this.props.asin} />
+				<CommentList commentsToShow={this.state.comments} />
+			</div>
+		);
+	}
 }
 
-export default CommentArea
+export default CommentArea;
